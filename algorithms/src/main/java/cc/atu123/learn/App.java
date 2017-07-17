ba1ea858cd8642680de3cad5dcc870aa5923498c
atu <wanglichong2010@163.com>
2017-07-17 08:33:50 +0800

    sort

--- algorithms/src/main/java/cc/atu123/learn/App.java
+++ algorithms/src/main/java/cc/atu123/learn/App.java
@@ -1,5 +1,8 @@
 package cc.atu123.learn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println(Math.log(1000)/Math.log(2));
         System.out.println( "Hello World!" );
+        List list = new ArrayList<String>();
+
     }
 }
