6420f619a5dd6a9a2cd894fe2bf3448ee78f4493
atu123 <wanglichong2010@163.com>
2017-07-17 15:12:58 +0800

    1、归并排序 2、分治法解决filter、不用外部变量，过滤数组问题

--- algorithms/src/main/java/cc/atu123/learn/algorithms/recursion/FilterService.java
+++ algorithms/src/main/java/cc/atu123/learn/algorithms/recursion/FilterService.java
@@ -1,7 +1,5 @@
 package cc.atu123.learn.algorithms.recursion;
 
-import java.util.Arrays;
-
 /**
  * Created with IDEA
  * User: atu
@@ -9,30 +7,86 @@ import java.util.Arrays;
  * Time: 15:34
  */
 public class FilterService{
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 2, 5, 6, 0, 2, 0, 1, 4};
-        System.out.println(Arrays.toString(array));
-        System.out.println("\nfilter result:");
-        System.out.println(Arrays.toString( FilterArrayByFilter(array, new NotZeroFilter())));
+        int []result =  FilterArrayByFilter(array, new NotZeroFilter());
+        print("result ",result);
     }
-    static int[] FilterArrayByFilter(int[] arr, Filter f){
-        //Arrays.stream();
-        for (int i : arr) {
-            
+
+
+    /**
+     * Filter array by filter int [ ].
+     *
+     * @param arr the arr
+     * @param f   the f
+     * @return the int [ ]
+     */
+    static int [] FilterArrayByFilter(int[] arr, Filter f){
+        if (arr.length==1) {
+            if(f.test(arr[0]))
+                return arr;
+            else
+                return null;
+        }else {
+            int cutor = arr.length / 2-1;
+            int[] tmp1 = FilterArrayByFilter(subArray(arr,0,cutor), f);
+            int[] tmp2 = FilterArrayByFilter(subArray(arr,cutor+1,arr.length-1), f);
+            return concatArray(tmp1,tmp2);
         }
-        return FilterArrayByFilter(arr,f);
     }
+
+    /**
+     * The type Not zero filter.
+     */
     static class NotZeroFilter implements Filter{
         public boolean test(int i) {
             if(i>0){
                 return true;
             }
-
             return false;
         }
     }
+
+    /**
+     * The interface Filter.
+     */
     interface Filter {
         boolean test(int i);
+    }
+
+    public static void print(String title,int [] array){
+        System.out.println(title);
+        for (int integer : array) {
+            System.out.print(integer+"\t");
+        }
+        System.out.println();
+    }
+
+    static int[] concatArray(int[] a, int[] b) {
+        int[] c;
+        if(a==null&&b==null){
+            return null;
+        }else if(b==null){
+            return a;
+        }else if(a==null){
+            return b;
+        }else{
+            c = new int[a.length+b.length];
+            System.arraycopy(a, 0, c, 0, a.length);
+            System.arraycopy(b, 0, c, a.length, b.length);
+            return c;
+        }
+    }
+
+    static int[] subArray(int[] a, int begin, int end) {
+        int[] c= new int[end-begin+1];
+        System.arraycopy(a, begin, c, 0,c.length);
+        return c;
     }
 }
 
---
+++ algorithms/src/main/java/cc/atu123/learn/algorithms/sort/GuibingSort.java
@@ -0,0 +1,78 @@
+package cc.atu123.learn.algorithms.sort;
+
+/**
+ * Created by lichongwang on 2017/7/17.
+ */
+public class GuibingSort {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        int [] array = {8,2,5,3,4,6,7,1,9,0};
+        print("原数组：",array);
+        sort(array,0,array.length-1);
+        print("排序后：",array);
+    }
+
+    /**
+     * Sort.
+     *
+     * @param array the array
+     * @param begin the begin
+     * @param end   the end
+     */
+    public static void sort(int [] array,int begin,int end){
+        if(begin>=end){
+            return;
+        }
+        int cuter = (begin+end)/2;
+        System.out.println("cuter: "+cuter);
+        sort(array,begin,cuter);
+        sort(array,cuter+1,end);
+        merge(array, begin ,cuter, end);
+    }
+
+    /**
+     * Merge.
+     *
+     * @param array the array
+     * @param begin the begin
+     * @param cuter the cuter
+     * @param end   the end
+     */
+    public static void merge(int [] array, int begin, int cuter, int end){
+        int[] tmp = array.clone();
+        int pointer1 = begin;
+        int pointer2 = cuter+1;
+        for (int i = begin;i<=end;i++){
+            if(pointer1<=cuter && pointer2<=end){
+                if (tmp[pointer1] <= tmp[pointer2]) {
+                    array[i] = tmp[pointer1++];
+                }else{
+                    array[i] = tmp[pointer2++];
+                }
+            }else if(pointer1<=cuter){
+                array[i] = tmp[pointer1++];
+            }else if(pointer2<=cuter){
+                array[i] = tmp[pointer2++];
+            }
+        }
+    }
+
+    /**
+     * Print.
+     *
+     * @param title the title
+     * @param array the array
+     */
+    public static void print(String title,int [] array){
+        System.out.println(title);
+        for (int integer : array) {
+            System.out.print(integer+"\t");
+        }
+        System.out.println();
+    }
+
+}
