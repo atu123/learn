0043b9ff598190c0c6d389ba09f7896d31e39e20
atu123 <wanglichong2010@163.com>
2017-07-19 18:49:29 +0800

    heap sort init

---
+++ algorithms/src/main/java/cc/atu123/learn/algorithms/sort/HeapSort.java
@@ -0,0 +1,17 @@
+package cc.atu123.learn.algorithms.sort;
+
+/**
+ * Created by lichongwang on 2017/7/19.
+ */
+public class HeapSort extends SortBase {
+    public static void main(String[] args) {
+        int [] array = {8,2,5,3,4,6,7,1,9,0};
+        print("原数组：",array);
+        sort(array);
+        print("排序后：",array);
+    }
+
+    public static void sort(int[] array){
+
+    }
+}
