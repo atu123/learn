41099f692198f6922b16e4837e267d641d99a607
atu <wanglichong2010@163.com>
2017-07-30 21:38:24 +0800

    优先队列

---
+++ algorithms/src/main/java/cc/atu123/learn/algorithms/datastructures/PriorityQueue.java
@@ -0,0 +1,27 @@
+package cc.atu123.learn.algorithms.datastructures;
+
+/**
+ * Created by atu on 2017/7/26.
+ */
+public class PriorityQueue {
+    private int[] array;
+    public void insert(int value){
+
+    }
+
+    public int maxmum(){
+        return 0;
+    }
+
+    public int extractMax(){
+        return array[0];
+    }
+
+    public void increaseKey(int index, int value){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
